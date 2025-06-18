288321fec79d775e8b96c84697b43de8f63b2ab5
kir_exp <asdsad@asd.asd>
2025-06-18 13:28:41 +0500

    feat: rename test

--- EnergyChecker/EnergyChecker.Tests/MeterReadingProcessorTests.cs
+++ EnergyChecker/EnergyChecker.Tests/MeterReadingProcessorTests.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EnergyChecker.Tests;
 
-public class UnitTest1
+public class MeterReadingProcessorTests
 {
     private readonly Fixture _fixture = new();
 
